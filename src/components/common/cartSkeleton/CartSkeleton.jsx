321a367da39c0726df0abf7413964486ae02a328
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-11-11 23:53:28 -0300

    error

--- src/components/common/cartSkeleton/CartSkeleton.jsx
+++ src/components/common/cartSkeleton/CartSkeleton.jsx
@@ -1,38 +1,38 @@
-import { Skeleton } from "@mui/material"
+// import { Skeleton } from "@mui/material"
 
 
-const CartSkeleton = () => {
+// const CartSkeleton = () => {
   
   
-  return (
-    <div style={{ display: "flex", gap: 20, padding: 32 }}>
-    <div>
-    <Skeleton variant="rectangular" width={250} height={118} />
-    <Skeleton width="20%" />
-    <Skeleton width="30%" />
-    <Skeleton variant="text" width={40} height={40} />
-  </div>
-  <div>
-    <Skeleton variant="rectangular" width={250} height={118} />
-    <Skeleton width="20%" />
-    <Skeleton width="30%" />
-    <Skeleton variant="text" width={40} height={40} />
-  </div>
-  <div>
-    <Skeleton variant="rectangular" width={250} height={118} />
-    <Skeleton width="20%" />
-    <Skeleton width="30%" />
-    <Skeleton variant="text" width={40} height={40} />
-  </div>
-  <div>
-    <Skeleton variant="rectangular" width={250} height={118} />
-    <Skeleton width="20%" />
-    <Skeleton width="30%" />
-    <Skeleton variant="text" width={40} height={40} />
-  </div>
+//   return (
+//     <div style={{ display: "flex", gap: 20, padding: 32 }}>
+//     <div>
+//     <Skeleton variant="rectangular" width={250} height={118} />
+//     <Skeleton width="20%" />
+//     <Skeleton width="30%" />
+//     <Skeleton variant="text" width={40} height={40} />
+//   </div>
+//   <div>
+//     <Skeleton variant="rectangular" width={250} height={118} />
+//     <Skeleton width="20%" />
+//     <Skeleton width="30%" />
+//     <Skeleton variant="text" width={40} height={40} />
+//   </div>
+//   <div>
+//     <Skeleton variant="rectangular" width={250} height={118} />
+//     <Skeleton width="20%" />
+//     <Skeleton width="30%" />
+//     <Skeleton variant="text" width={40} height={40} />
+//   </div>
+//   <div>
+//     <Skeleton variant="rectangular" width={250} height={118} />
+//     <Skeleton width="20%" />
+//     <Skeleton width="30%" />
+//     <Skeleton variant="text" width={40} height={40} />
+//   </div>
 
-</div>
-  )
-}
+// </div>
+//   )
+// }
 
-export default CartSkeleton
+// export default CartSkeleton
