b10dd1b7618925153e37ab4e6c26d64588f3c533
william jose anez <WILLIAMJOSEANEZ@GMAIL.COM>
2023-11-13 20:38:11 -0300

    haciendolo responsive

--- src/components/pages/itemListContainer/ItemList.jsx
+++ src/components/pages/itemListContainer/ItemList.jsx
@@ -3,7 +3,7 @@ import "./error.css";
 
 const ItemList = ({ items }) => {
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', padding:26, margin:15 }}>
       {items.map((item) => {
         return <ProductCard key={item.id} item={item} />;
       })}
